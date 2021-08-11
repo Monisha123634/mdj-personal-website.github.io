2b558b4d62f12254ff1e6ee024822059206b9c67
pooja_dj <pooja_dj@dell.com>
2021-08-11 19:59:48 +0530

    My first website

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mystery_code__blog_9.Startup))]
+namespace mystery_code__blog_9
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
